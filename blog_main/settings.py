1c8538a5255aac28bde183a97d5004b89dbb86ae
mr-Thamariaselvam <thamaraiselvamc@gmail.com>
2025-11-12 10:38:51 +0530

    Update ALLOWED_HOSTS for deployment

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-n8^@1$ll9wyo)$h5h+5xq-a7a!vnn&@5(gi#!3*9#t@=nqc&al
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['https://citizen-rights.onrender.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['citizen-rights.onrender.com', 'localhost', '127.0.0.1']
 
 
 
